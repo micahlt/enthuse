a2f43695335986b7d62928604a904f309b8d2cd0
micahlt <micahincorporated@gmail.com>
2020-05-12 18:18:40 -0500

    Began implementing asset production

--- cmds/build.js
+++ cmds/build.js
@@ -5,11 +5,12 @@ module.exports = (args) => {
   console.log('⚡ Preparing to build...');
   fs.access('enthuse-config.json', (err) => {
     if (err) {
-      console.log("⛔ ERROR: Couldn't find an enthuse-config.json file.".red);
+      console.error();
+      ("⛔ ERROR: Couldn't find an enthuse-config.json file.".red);
     } else {
       fs.readFile('enthuse-config.json', 'utf8', function(err, data) {
         if (err) {
-          console.error("⛔ ERROR: Invalid enthuse-config.json file");
+          console.error("⛔ ERROR: Invalid enthuse-config.json file".red);
         }
         var validator = new ZSchema();
         var schema = {
@@ -17,7 +18,53 @@ module.exports = (args) => {
         };
         var valid = validator.validate(JSON.parse(data), schema);
         if (valid) {
-          console.log("✅ Valid enthuse-config.json file");
+          console.log("✔ Valid enthuse-config.json file");
+          var config = JSON.parse(data);
+          var css = './css';
+          if (!fs.existsSync(css)) {
+            fs.mkdirSync(css);
+          }
+          console.log("✔ Verified that the ./css folder exists");
+          var js = './js';
+          if (!fs.existsSync(js)) {
+            fs.mkdirSync(js);
+          }
+          console.log("✔ Verified that the ./js folder exists");
+          var img = './img';
+          if (!fs.existsSync(img)) {
+            fs.mkdirSync(img);
+          }
+          console.log("✔ Verified that the ./img folder exists");
+          let htmlToWrite =
+            `
+            <!DOCTYPE html>
+            <html lang="en" dir="ltr">
+            <head>
+              <meta name="description" content="` +
+            config["platform-description"] +
+            `">
+              <meta charset="utf-8">
+              <title>` +
+            config["platform-name"] +
+            `</title>
+              <link rel="stylesheet" href="css/home-layout.css">
+              <link rel="stylesheet" href="/css/custom.css">
+            </head>
+            <body>
+              <div class="enthuse-navbar">
+                <img class="enthuse-logo">
+              </div>
+              <div class="enthuse-main">
+              </div>
+              <script src="js/home.js" charset="utf-8"></script>
+            </body>
+            </html>
+            `;
+          console.log("✔ Successfully generated HTML syntax")
+          fs.writeFile(config["platform-name"].toLowerCase() + ".html", htmlToWrite, function(err) {
+            if (err) return console.log(err);
+            console.log("✔ Successfully created HTML document as " + config["platform-name"].toLowerCase() + ".html");
+          });
         } else {
           console.error("⛔ ERROR: Invalid enthuse-config.json file");
         }
